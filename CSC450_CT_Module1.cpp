06013f89262cec44c7d8c1c916f27c3b9c425d4d
jgweaver <157259718+jgweaver@users.noreply.github.com>
2025-05-24 19:29:52 -0600

    Add files via upload

---
+++ CSC450_CT_Module1.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : CSC450_CT_Module1.cpp
+// Author      : Weaver, Jonathan
+// Version     :
+// Description : Outputs the fictional person's information
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	//Displays the information for the fictional person
+	cout << "First Name: Donny" << endl << "Last Name: Stegging" << endl << "Street Address: 5074 Ursula St" << endl << "City: Galveston" << endl << "Zip Code: 40867" << endl;
+
+	return 0;
+}
